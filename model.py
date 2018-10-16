a7ec9734e4bed5de555c9671fedcb061bfd6e188
Eliezer de Souza da Silva <eddasilv@samuel02.idi.ntnu.no>
2018-10-16 15:34:53 +0200

    initial model without optimizer

--- model.py
+++ model.py
@@ -6,19 +6,67 @@ import torch
 from torch import nn
 
 
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes, n_layers=2):
-        super(RNN, self).__init__()
+class Composer(nn.Module):
+    def __init__(self, input_size, hidden_size, num_tags, n_layers=2):
+        super(Composer, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.num_classes = num_classes
+        self.num_tags = num_tags
         self.n_layers = n_layers
         
-        self.class_embedding = nn.Embedding(num_classes,hidden_size)
-        
+        self.tags_embedding = nn.Embedding(num_tags,hidden_size)
         self.notes_encoder = nn.Linear(in_features=input_size, out_features=hidden_size)
-       
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        self.notes_decoder = nn.Linear(in_features=hidden_size, out_features=input_size)
+        self.output= nn.Sigmoid()
     
-    
-    def forward(self, input_sequences, input_sequences_lengths, hidden=None):
+    def forward(self, input_sequence, tag, hidden=None):
+        if(hidden is None):
+            hidden = self.tags_embedding(tag)
+            hidden = torch.cat( tuple(hidden for i in range(self.n_layers))) 
+        output, hidden = self.gru(self.notes_encoder(input_sequence),hidden)
+        output = self.output(self.notes_decoder(output))
+        return output,hidden
+
+class ComposerLSTM_C(nn.Module):
+    r""" TODO: change to LSTM with embedding entering in the initial cell state c
+    """
+    def __init__(self, input_size, hidden_size, num_tags, n_layers=2):
+        super(ComposerLSTM_C, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_tags = num_tags
+        self.n_layers = n_layers
+        
+        self.tags_embedding = nn.Embedding(num_tags,hidden_size)
+        self.notes_encoder = nn.Linear(in_features=input_size, out_features=hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        self.notes_decoder = nn.Linear(in_features=hidden_size, out_features=input_size)
+        
+    def forward(self, input_sequence, tag, hidden=None):
+        if(hidden is None):
+            hidden = self.tags_embedding(tag)
+            hidden = torch.cat( (hidden for i in range(self.n_layers))) 
+
+
+class ComposerLSTM_H(nn.Module):
+       r""" TODO: change to LSTM with embedding entering in the initial hidden state h
+    """
+    def __init__(self, input_size, hidden_size, num_tags, n_layers=2):
+        super(ComposerLSTM_H, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_tags = num_tags
+        self.n_layers = n_layers
+        
+        self.tags_embedding = nn.Embedding(num_tags,hidden_size)
+        self.notes_encoder = nn.Linear(in_features=input_size, out_features=hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        self.notes_decoder = nn.Linear(in_features=hidden_size, out_features=input_size)
+        
+    def forward(self, input_sequence, tag, hidden=None):
+        if(hidden is None):
+            hidden = self.tags_embedding(tag)
+            hidden = torch.cat( (hidden for i in range(self.n_layers))) 
+
 
