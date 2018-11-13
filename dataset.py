fc556763a833b938c284a36a71b3d415016a1025
Eliezer de Souza da Silva <eddasilv@samuel02.idi.ntnu.no>
2018-11-13 14:36:11 +0100

    visualize in dataset

--- dataset.py
+++ dataset.py
@@ -43,7 +43,7 @@ class pianoroll_dataset_batch(Dataset):
         return datp.get_numkeys(self.data)[0]
         
     def view_pianoroll(self,idx):
-        datp.visualize_piano_roll(self[idx].squeeze(1).numpy().astype(float))
+        datp.visualize_piano_roll(self[idx][0].squeeze(1).numpy().astype(float))
         
 class pianoroll_dataset_chunks(Dataset):
     def __init__(self, root_dir,transform=None,binarize=True,delta=1):
@@ -96,7 +96,7 @@ class pianoroll_dataset_chunks(Dataset):
         return datp.get_numkeys(self.fulldata)[0]
         
     def view_pianoroll(self,idx):
-        datp.visualize_piano_roll(self[idx].squeeze(1).numpy().astype(float))
+        datp.visualize_piano_roll(self[idx][0].squeeze(1).numpy().astype(float))
         
 def one_end(input_tensor,k=1):
     return torch.cat( (input_tensor[k:], torch.zeros(size=(k,input_tensor.shape[1],input_tensor.shape[2]))) )
